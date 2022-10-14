6f1e48fed6dcad86bdf3b4343575e8fe6023c1cf
Ailyn del Pino Acosta <ailyndpa@gmail.com>
2022-10-13 23:06:21 -0300

    Uploading test case 3

--- src/test/java/TestCases/TestCase1.java
+++ src/test/java/TestCases/TestCase1.java
@@ -2,11 +2,11 @@ package TestCases;
 
 import org.testng.annotations.Test;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Properties;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -15,10 +15,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
-
 
 public class TestCase1 {
 	protected WebDriver driver;
@@ -41,9 +40,11 @@ public class TestCase1 {
 		driver.get(url);
 		driver.manage().window().maximize();
 	}
-
 	@Test
 	public void contactUs() throws InterruptedException, FileNotFoundException, IOException {
+		File doc = new File(".\\src\\test\\resources\\invalid address.txt");
+		BufferedReader obj = new BufferedReader(new FileReader(doc));
+		
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("window.scroll(0,1000)");
 		driver.findElement(By.xpath("//button[@type='button']/span[text()='Contact us']")).click();
@@ -51,17 +52,18 @@ public class TestCase1 {
 		driver.findElement(By.cssSelector("input[name='your-subject']")).sendKeys("First test");
 		driver.findElement(By.cssSelector("textarea[name='your-message']")).sendKeys("This is the message of the first test");
 		
-		/*4) Para el caso de prueba 1, prepare un archivo de datos de prueba en un formato de su elección, 
-		el archivo debe contener 5 conjuntos de direcciones de correo electrónico no válidas. Realice la 
-		prueba para que se ejecute 5 veces con cada dirección de correo electrónico.*/
-		
-		
-		driver.findElement(By.xpath("//input[@type='email']")).sendKeys("usuario.com");
-		driver.findElement(By.xpath("//input[@type='submit']")).click();
-		String errorMessage=driver.findElement(By.xpath("//span[@class='wpcf7-not-valid-tip']")).getText();
-		Assert.assertTrue(errorMessage.equals("The e-mail address entered is invalid."), "The error message displayed is not as expected.");
-		driver.close();
+		  String currentAddress;
+		  while ((currentAddress = obj.readLine()) != null) {
+			driver.findElement(By.xpath("//input[@type='email']")).sendKeys(currentAddress);
+			driver.findElement(By.xpath("//input[@type='submit']")).click();
+			Thread.sleep(3000);
+			String errorMessage = driver.findElement(By.xpath("//span[@class='wpcf7-not-valid-tip']")).getText();
+			Assert.assertTrue(errorMessage.equals("The e-mail address entered is invalid."),"The error message displayed is not as expected.");
+			driver.findElement(By.xpath("//input[@type='email']")).clear();
+		  }		 
 	}
-	
-
+	@AfterClass
+	public void After() {
+		 driver.close();
+	}
 }
